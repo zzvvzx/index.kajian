9a0759cc5eaf16d345f8e581aaf6655a4aeb4e4f
z <z@unknown.org>
2019-09-19 17:02:23 +0900

    z

--- script/home.js
+++ script/home.js
@@ -6,6 +6,9 @@ const ROOT = "/storage/sdcard0/Download/zaman.old/data/";
 const DIR  = fs.readdirSync(ROOT);
 const PATH_out = ROOT + "../index.html";
 
+function time(x){ var h=~~(x/3600),m=~~(x/60)%60,s=~~(x)%60; return (h?(h<10?"0":"")+h+":":"")+((m<10)&&h?"0":"")+m+":"+(s<10?"0":"")+s };
+
+
 let html = "";
 
 for(let i=0; i<DIR.length; i++){
@@ -16,7 +19,7 @@ for(let i=0; i<DIR.length; i++){
 <li>
   <span>${i+1}</span>
   <span><a href="data/${d}/">${d}</a></span>
-  <span>${META.date} - ${META.total_size}Mb / ${META.total_items} items (${META.total_duration} hours)</span>
+  <span>${META.date} / ${time(parseFloat(META.total_duration)*3600)} ${META.total_size}Mb</span>
 </li>`;
 };
 
